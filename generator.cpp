c5306bb3aeefd6c3b607454d295638c98a65e2e3
fortyfourjs <dincaionut@hotmail.com>
2025-06-15 02:23:08 +0300

    mutat tot clutterul din main intr-o functie separata

--- generator.cpp
+++ generator.cpp
@@ -7,6 +7,7 @@ u_int16_t fct_lfsr(u_int16_t lfsr)
     u_int16_t bit = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1;
     return (lfsr >> 1) | (bit << 15);
 }
+
 int valori_min_alfabetice()
 {
     int min_alfabetice;
@@ -103,7 +104,7 @@ int alegere_nr_speciale(int lfsr, int min_speciale, int max_speciale)
 int nr_parole_generate()
 {
     int nr_parole;
-    std::cout << "Cate parole doresti sa generezi?" << '\n';
+    std::cout << "Cate parole vrei sa generezi?" << '\n';
     std::cin >> nr_parole;
     
     return nr_parole;
@@ -124,87 +125,59 @@ std::string fct_separator(std::string parola, char separator, int nr_separator)
     return parola_finala;
 }
 
-int main()
+void generare_parola(int min_alfabetice, int max_alfabetice, int min_numerice, int max_numerice, int min_speciale, int max_speciale, 
+                            int total_caractere, int nr_separator, char separator, int nr_parole)
 {
-    std::vector<int> litere;
-    std::vector<int> cifre;
-    std::vector<int> speciale;
+    std::vector<int> litere, cifre, speciale;
 
     for(int i = 65; i <= 90; i++) litere.push_back(i);
-
     for(int i = 97; i<= 122; i++) litere.push_back(i);
-
     for(int i = 48; i <= 57; i++) cifre.push_back(i);
-
     speciale.insert(speciale.end(), {33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 58, 59, 60, 61, 62, 63, 64, 94, 95, 96});
 
-    int min_alfabetice = valori_min_alfabetice();
-    int max_alfabetice = valori_max_alfabetice();
-    int min_numerice = valori_min_numerice();
-    int max_numerice = valori_max_numerice();
-    int min_speciale = valori_min_speciale();
-    int max_speciale = valori_max_speciale();
-    int total_caractere = valori_total_caractere();
-    int nr_separator = interval_separator();
-    char separator = '-';
-    
-    /* u_int16_t lfsr = 0xACE1u; */ // seed initial, aceleasi rezultate 
-    u_int16_t lfsr = static_cast<u_int16_t>(time(nullptr)); //seedul devine ora actuala in secunde.rezultatele difera de fiecare data
+    /* u_int16_t lfsr = 0xACE1u; */ // seed initial, aceleasi rezultate
+    u_int16_t lfsr = static_cast<u_int16_t>(time(nullptr)); //seedul devine ora actuala in secunde, rezultatele difera de fiecare data
     int nr_alfabetice = alegere_nr_alfabetice(lfsr, min_alfabetice, max_alfabetice);
     int nr_numerice = alegere_nr_numerice(lfsr, min_numerice, max_numerice);
-    int nr_speciale = total_caractere - nr_alfabetice - nr_numerice;
+    int nr_speciale = total_caractere - nr_alfabetice - nr_numerice; // 
 
-    if(nr_speciale < min_speciale || nr_speciale > max_speciale)
+    for(int i = 0; i < nr_parole; i++)
     {
-        std::cout << "incearca alta combinatie" << '\n';
+        int nr_alfabetice = alegere_nr_alfabetice(lfsr, min_alfabetice, max_alfabetice);
+        lfsr = fct_lfsr(lfsr);
+        int nr_numerice = alegere_nr_numerice(lfsr, min_numerice, max_numerice);
+        lfsr = fct_lfsr(lfsr);
+        int nr_speciale = total_caractere - nr_alfabetice - nr_numerice;
 
-        return 1;
-    }
-
-    if(total_caractere < min_alfabetice + min_numerice + min_speciale)//erori introducere date
-    {
-        std::cout << "Valorile minime introduse depasesc lungimea parolei \n";
-
-        return 1;
-    }
-    if(total_caractere > max_alfabetice + max_numerice + max_speciale)//erori introducere date
-    {
-        std::cout << "Lungimea parolei este mai mare decat valorile maxime introduse \n";
-
-        return 1;
-    }
+        if(nr_speciale < min_speciale || nr_speciale > max_speciale)
+        {
+            std::cout<<"incearca alta comibnatie" << '\n';
+            continue;
+        }
     
-    int nr_parole = nr_parole_generate();
     
-  
-    for(int i = 0; i < nr_parole; ++i)
-    {
-    std::string parola_alfabetice;
-    std::string parola_numerice;
-    std::string parola_speciale;
-
+    std::string parola;
+    
     for(int i = 0; i < nr_alfabetice; i++)
     {
         lfsr = fct_lfsr(lfsr);
         char ch = static_cast<char>(litere[lfsr % litere.size()]);
-        parola_alfabetice += ch;
+        parola += ch;
     }
 
     for(int i = 0; i < nr_numerice; i++)
     {
         lfsr = fct_lfsr(lfsr);
         char ch = static_cast<char>(cifre[lfsr % cifre.size()]);
-        parola_numerice += ch;
+        parola += ch;
     }
 
     for(int i = 0; i < nr_speciale; i++)
     {
         lfsr = fct_lfsr(lfsr);
         char ch = static_cast<char>(speciale[lfsr % speciale.size()]);
-        parola_speciale += ch;
+        parola += ch;
     }
-
-    std::string parola = parola_alfabetice + parola_numerice + parola_speciale; 
 
     for (int i = parola.size() - 1; i > 0; i--) //lfsr pe parola finala
     {
@@ -218,5 +191,40 @@ int main()
     std::string parola_finala = fct_separator(parola, separator, nr_separator);
 
     std::cout << "parola nr." << i + 1 << ": "<< parola_finala << '\n';
-    }  
+    }
+}
+  
+
+int main()
+{
+
+    int min_alfabetice = valori_min_alfabetice();
+    int max_alfabetice = valori_max_alfabetice();
+    int min_numerice = valori_min_numerice();
+    int max_numerice = valori_max_numerice();
+    int min_speciale = valori_min_speciale();
+    int max_speciale = valori_max_speciale();
+    int total_caractere = valori_total_caractere();
+    int nr_separator = interval_separator();
+    char separator = '-';
+    int nr_parole = nr_parole_generate();
+    
+    
+    if(total_caractere < min_alfabetice + min_numerice + min_speciale)//erori introducere date
+    {
+        std::cout << "Valorile minime introduse depasesc lungimea parolei \n";
+
+        return 1;
+    }
+    if(total_caractere > max_alfabetice + max_numerice + max_speciale)//erori introducere date
+    {
+        std::cout << "Lungimea parolei este mai mare decat valorile maxime introduse \n";
+
+        return 1;
+    }
+    
+    generare_parola(min_alfabetice, max_alfabetice, min_numerice, max_numerice, min_speciale, max_speciale, total_caractere, nr_separator, separator, nr_parole);
+    
+  
+    
 }
